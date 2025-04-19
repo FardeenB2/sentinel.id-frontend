64f7e941cb369e99d4090216da02854f10fb0e3d
Fardeen Bhuiyan <fardeenbhuiyantwo@gmail.com>
2025-04-18 23:55:42 -0400

    fixed opacity issue with background design

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -14,42 +14,47 @@ function Login() {
         <div className="techbodyparent">
             <div className="techsidebodyleft"> </div>
             <div className="techsidebodyright"> </div>
+            
         </div>
 
         <div className="body">
-            <div className = "centercircuitbg">           
-                
-                <div className = "inputs">
-                    <input id="usernameInputBox" placeholder="Username/Email Address"/>  
-                    <input id="passwordInputBox" placeholder="Password"/>
-                </div>
+            <div className = "centercircuitbg">
 
+         
+                <div className="content">
 
-                <div className = "memory">
-                    <div className = "remember">
-                    <input type="checkbox" id="rememberMeBOX"/>
-                    <label htmlFor="rememberMe" id="rememberMe"> Remember Me</label>
+                    <div className = "inputs">
+                        <input id="usernameInputBox" placeholder="Username/Email Address"/>  
+                        <input id="passwordInputBox" placeholder="Password"/>
                     </div>
-                    <Link to= "/forgotpw" id="forgotPass"> Forgot Password?</Link>
-                </div>
 
 
-                <div className = "log">
-
-                    <Link to= "/login">
-                    <button type="button" id="logbutton"> Log-In </button> 
-                    </Link> 
-                        <p className="orLoginWith"> Or log in with:</p><br></br>
-
-                        <div className ="loginOptions"> 
-                        <img src= {facebookLogo} alt="facebook logo"></img>
-                        <img src= {linkedin} alt="linkedin logo"></img>
-                        <img src= {googleLogo} alt="google logo"></img>
+                    <div className = "memory">
+                        <div className = "remember">
+                        <input type="checkbox" id="rememberMeBOX"/>
+                        <label htmlFor="rememberMe" id="rememberMe"> Remember Me</label>
                         </div>
+                        <Link to= "/forgotpw" id="forgotPass"> Forgot Password?</Link>
+                    </div>
 
-                <p className="newUser">New User?</p>
-                <Link to= "/register" id="signup">Sign up for free</Link>
 
+                    <div className = "log">
+
+                        <Link to= "/login">
+                        <button type="button" id="logbutton"> Log-In </button> 
+                        </Link> 
+                            <p className="orLoginWith"> Or log in with:</p><br></br>
+
+                            <div className ="loginOptions"> 
+                            <img src= {facebookLogo} alt="facebook logo"></img>
+                            <img src= {linkedin} alt="linkedin logo"></img>
+                            <img src= {googleLogo} alt="google logo"></img>
+                            </div>
+
+                    <p className="newUser">New User?</p>
+                    <Link to= "/register" id="signup">Sign up for free</Link>
+
+                    </div>
                 </div>
             </div>
         </div>
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -13,34 +13,35 @@ function Register() {
         </div>
         
         <div className="body">
-        <div className = "centercircuitbg">           
+            <div className = "centercircuitbg">           
+                <div className = "content">
+                    <div className = "inputs">
+                        <input id="usernameInputBox" placeholder="New Username/Email Address"/>  
+                    </div>
 
-            <div className = "inputs">
-                <input id="usernameInputBox" placeholder="New Username/Email Address"/>  
-            </div>
+                    <div className = "newinputs">
+                        <input id="newpwbox" placeholder="New Password"/>
+                        <input id="confirmpwbox" placeholder="Confirm Password"/>
+                    </div>
 
-            <div className = "newinputs">
-                <input id="newpwbox" placeholder="New Password"/>
-                <input id="confirmpwbox" placeholder="Confirm Password"/>
-            </div>
+                    <div className = "memory">
+                        <div className = "remember">
+                        <input type="checkbox" id="rememberMeBOX"/>
+                        <label htmlFor="rememberMe" id="rememberMe"> Remember Me</label>
+                        </div>
+                    </div>
 
-            <div className = "memory">
-                <div className = "remember">
-                <input type="checkbox" id="rememberMeBOX"/>
-                <label htmlFor="rememberMe" id="rememberMe"> Remember Me</label>
+
+                    <div className = "register">
+
+                        <button type="button" id="logbutton"> Register</button>
+
+                    </div>
+
+                    <p className="have">Have an Account?</p>
+                    <Link to= "/login" id="loginhere">Log in here</Link>
                 </div>
             </div>
-
-
-            <div className = "register">
-
-                <button type="button" id="logbutton"> Register</button>
-
-            </div>
-
-            <p className="have">Have an Account?</p>
-            <Link to= "/login" id="loginhere">Log in here</Link>
-        </div>
         </div>
     </>
 
